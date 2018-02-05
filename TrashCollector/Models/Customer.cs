d793e8ca0d54a413f9314c6c49437354059a0328
Dylan <dquesada186@gmail.com>
2018-02-05 07:40:08 -0600

    Initial Commit

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
