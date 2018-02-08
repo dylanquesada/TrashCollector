5125eb09df30ec361f8e0c5eb72253de877f7e0e
Dylan <dquesada186@gmail.com>
2018-02-08 07:55:38 -0600

    Added purse class to project.

---
+++ TrashCollector/Helpers/Purse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Helpers
+{
+    public class Purse
+    {
+        // Member Variables
+        private decimal RubbishRemover3KFunds;
+        private decimal StartupCash = 300;
+        private object thisLock = new object();
+        // Constructor
+        public Purse()
+        {
+            RubbishRemover3KFunds = StartupCash;
+        }
+        // Member Methods
+        public void CollectFromCustomer(decimal AmountPayed)
+        {
+            lock (thisLock)
+            {
+                RubbishRemover3KFunds += AmountPayed;
+            }
+        }
+    }
+}
