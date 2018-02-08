052c8df10be69bc60ae0595e0296ebc74e6e19a9
Dylan <dquesada186@gmail.com>
2018-02-08 08:09:55 -0600

    Wrote a sweet formula / method to calculate monthly cost based on dateTime.

---
+++ TrashCollector/Helpers/Bill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Helpers
+{
+    public class Bill
+    {
+        private decimal MonthlyCost;
+
+        public decimal GetMonthlyCharge(decimal charge, DayOfWeek pickupDay)
+        {
+            decimal total = 0;
+            DayOfWeek today = DateTime.Now.DayOfWeek;
+            for(int i = 0; i < DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month), i++)
+            {
+                if(pickupDay == new DateTime(DateTime.Now.Year, DateTime.Now.Month, i).DayOfWeek)
+                {
+                    total += charge;
+                }
+            }
+            return charge;
+        }
+
+    }
+}
