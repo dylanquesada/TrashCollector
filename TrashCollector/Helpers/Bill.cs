6310a16fadefbb38424ff9d5b80bb7a0f847af37
Dylan <dquesada186@gmail.com>
2018-02-08 08:33:42 -0600

    Added method to calcualte for a vacation and remove cost from bill

--- TrashCollector/Helpers/Bill.cs
+++ TrashCollector/Helpers/Bill.cs
@@ -12,16 +12,25 @@ namespace TrashCollector.Helpers
         public decimal GetMonthlyCharge(decimal charge, DayOfWeek pickupDay)
         {
             decimal total = 0;
-            DayOfWeek today = DateTime.Now.DayOfWeek;
-            for(int i = 0; i < DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month), i++)
+            for (int i = 1; i <= DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month); i++)
             {
-                if(pickupDay == new DateTime(DateTime.Now.Year, DateTime.Now.Month, i).DayOfWeek)
+                if (pickupDay == new DateTime(DateTime.Now.Year, DateTime.Now.Month, i).DayOfWeek)
                 {
                     total += charge;
                 }
             }
-            return charge;
+            return total;
         }
-
+        public decimal AccountForVacation(decimal initial, decimal charge, DateTime start, DateTime end, DayOfWeek pickupDay)
+        {
+            for (int i = start.Day; i <= end.Day; i++)
+            {
+                if (pickupDay == new DateTime(DateTime.Now.Year, DateTime.Now.Month, i).DayOfWeek)
+                {
+                    initial -= charge;
+                }
+            }
+            return initial;
+        }
     }
 }
