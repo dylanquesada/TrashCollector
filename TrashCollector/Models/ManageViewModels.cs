f52b9e25667f424c199da7b493ce84b703cc9298
Dylan <dquesada186@gmail.com>
2018-02-08 08:51:31 -0600

    Added SetVacationDaysViewModel in manage

--- TrashCollector/Models/ManageViewModels.cs
+++ TrashCollector/Models/ManageViewModels.cs
@@ -6,6 +6,36 @@ using System;
 
 namespace TrashCollector.Models
 {
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string Number { get; set; }
+    }
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    }
     public class IndexViewModel
     {
         public bool HasPassword { get; set; }
@@ -13,20 +43,10 @@ namespace TrashCollector.Models
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
-        public List<string> PickupDays { get; set; }
-    }
-    public class SetPickupDaysViewModel
-    {
-        
-        [Required]       
-        public DayOfWeek PickupDay { get; set; }
         
     }
-    public class ManageLoginsViewModel
-    {
-        public IList<UserLoginInfo> CurrentLogins { get; set; }
-        public IList<AuthenticationDescription> OtherLogins { get; set; }
-    }
+
+
 
     public class FactorViewModel
     {
@@ -46,33 +66,31 @@ namespace TrashCollector.Models
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
-
-    public class ChangePasswordViewModel
+    public class SetPickupDaysViewModel
     {
         [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
-        public string OldPassword { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
-        public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public DayOfWeek PickupDay { get; set; }
     }
-
-    public class AddPhoneNumberViewModel
+    public class SetVacationDays
     {
         [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string Number { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Start Date")]
+        public DateTime StartDate;
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "End Date")]
+        public DateTime EndDate;
+    }    
+
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
     }
+
+
+
 
     public class VerifyPhoneNumberViewModel
     {
@@ -84,11 +102,5 @@ namespace TrashCollector.Models
         [Phone]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
-    }
-
-    public class ConfigureTwoFactorViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
     }
 }
