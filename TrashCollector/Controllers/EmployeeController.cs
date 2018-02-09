027c0a86bab61bb4182cf2324677000b9327795c
Dylan <dquesada186@gmail.com>
2018-02-08 18:20:12 -0600

    Added MVC for employee GetRoute -- ZipList*

---
+++ TrashCollector/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector.Models;
+
+
+namespace TrashCollector.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        public EmployeeController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public EmployeeController()
+        {
+
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: /Employee/GetZipList
+        public ActionResult GetZipList()
+        {
+            return View();
+        }
+        // POST /Employee/GetZipList
+        [HttpPost]
+        public ActionResult GetZipList(EmployeeViewModel model)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            if (ModelState.IsValid)
+            {
+                var list = new List<string>();
+                var query = from d in db.Users
+                            orderby d.Zip
+                            select d.Zip;
+                return View(list);
+            }
+            return View();
+        }
+    }
+}
---
+++ TrashCollector/Models/EmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class EmployeeViewModel
+    {
+    }
+    public class GetZipList
+    {
+        [Required]
+        public int Zip { get; set; }
+
+    }
+}
